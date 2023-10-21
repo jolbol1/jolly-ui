c0e6cbd0812df7d4e03e44a410bfd7b7b2003def
jolbol1 <github@jamesshopland.com>
2023-10-22 00:42:21 +0100

    fix: tooltip animations

--- apps/www/registry/default/ui/tooltip.tsx
+++ apps/www/registry/default/ui/tooltip.tsx
@@ -18,12 +18,13 @@ export interface TooltipProps extends RaTooltipProps {
 const Tooltip = React.forwardRef<
   React.ElementRef<typeof RaTooltip>,
   TooltipProps
->(({ className, offset = 4, ...props }, ref) => (
+>(({ className, offset = 4, crossOffset = -4, ...props }, ref) => (
   <RaTooltip
     ref={ref}
     offset={offset}
+    crossOffset={crossOffset}
     className={cn(
-      "z-50 overflow-hidden rounded-md border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+      "z-50 overflow-hidden rounded-md border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md animate-in fade-in-0 zoom-in-95 data-[exiting]:animate-out data-[exiting]:fade-out-0 data-[exiting]:zoom-out-95 data-[placement=bottom]:slide-in-from-top-2 data-[placement=left]:slide-in-from-right-2 data-[placement=right]:slide-in-from-left-2 data-[placement=top]:slide-in-from-bottom-2",
       className
     )}
     {...props}
