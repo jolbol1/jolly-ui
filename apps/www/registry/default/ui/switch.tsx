b7c38ab9219cb66206a2b750e1b4fcc37a72a67e
jolbol1 <github@jamesshopland.com>
2023-10-22 17:13:07 +0100

    fix: use client

--- apps/www/registry/default/ui/switch.tsx
+++ apps/www/registry/default/ui/switch.tsx
@@ -16,7 +16,7 @@ const Switch = React.forwardRef<React.ElementRef<typeof RaSwitch>, SwitchProps>(
   ({ className, ...props }, ref) => (
     <RaSwitch
       className={cn(
-        "peer inline-flex h-[24px] w-[44px] shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors data-[focus-visible]:outline-none data-[focus-visible]:ring-2 data-[focus-visible]:ring-ring data-[focus-visible]:ring-offset-2 data-[focus-visible]:ring-offset-background data-[disabled]:cursor-not-allowed data-[disabled]:opacity-50 data-[selected]:bg-primary bg-input",
+        "peer inline-flex h-[24px] w-[44px] shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent bg-input transition-colors data-[disabled]:cursor-not-allowed data-[selected]:bg-primary data-[disabled]:opacity-50 data-[focus-visible]:outline-none data-[focus-visible]:ring-2 data-[focus-visible]:ring-ring data-[focus-visible]:ring-offset-2 data-[focus-visible]:ring-offset-background",
         className
       )}
       {...props}
@@ -25,7 +25,7 @@ const Switch = React.forwardRef<React.ElementRef<typeof RaSwitch>, SwitchProps>(
       {({ isSelected }) => (
         <div
           className={cn(
-            "pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform translate-x-0",
+            "pointer-events-none block h-5 w-5 translate-x-0 rounded-full bg-background shadow-lg ring-0 transition-transform",
             { "translate-x-5": isSelected }
           )}
         />
