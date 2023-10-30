31e64a7febf0ec0382f01e72f3935eb90e6a39ae
jolbol1 <github@jamesshopland.com>
2023-10-30 14:54:04 +0000

    fix: tooltip and popover animations

--- apps/www/registry/default/ui/tooltip.tsx
+++ apps/www/registry/default/ui/tooltip.tsx
@@ -18,15 +18,14 @@ export interface TooltipProps extends RaTooltipProps {
 const Tooltip = React.forwardRef<
   React.ElementRef<typeof RaTooltip>,
   TooltipProps
->(({ className, offset = 4, crossOffset = -4, ...props }, ref) => (
+>(({ className, offset = 4, ...props }, ref) => (
   <RaTooltip
     ref={ref}
     offset={offset}
-    crossOffset={crossOffset}
     className={(values) =>
       cnv(
         values,
-        "z-50 overflow-hidden rounded-md border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md animate-in fade-in-0 zoom-in-95 data-[exiting]:animate-out data-[exiting]:fade-out-0 data-[exiting]:zoom-out-95 data-[placement=bottom]:slide-in-from-top-2 data-[placement=left]:slide-in-from-right-2 data-[placement=right]:slide-in-from-left-2 data-[placement=top]:slide-in-from-bottom-2",
+        "z-50 overflow-hidden rounded-md border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md animate-in fade-in-0 data-[exiting]:animate-out data-[exiting]:fade-out-0 data-[exiting]:zoom-out-95 data-[placement=bottom]:slide-in-from-top-2 data-[placement=left]:slide-in-from-right-2 data-[placement=right]:slide-in-from-left-2 data-[placement=top]:slide-in-from-bottom-2",
         className
       )
     }
--- apps/www/registry/new-york/ui/popover.tsx
+++ apps/www/registry/new-york/ui/popover.tsx
@@ -17,7 +17,7 @@ const Popover = React.forwardRef<
     className={(values) =>
       cnv(
         values,
-        "z-50 w-72 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[entering]:animate-in data-[exiting]:animate-out data-[entering]:fade-in-0 data-[exiting]:fade-out-0 data-[entering]:zoom-in-95 data-[exiting]:zoom-out-95 data-[placement=bottom]:slide-in-from-top-2 data-[placement=left]:slide-in-from-right-2 data-[placement=right]:slide-in-from-left-2 data-[placement=top]:slide-in-from-bottom-2",
+        "z-50 w-72 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[entering]:animate-in data-[exiting]:animate-out data-[entering]:fade-in-0 data-[exiting]:fade-out-0 data-[exiting]:zoom-out-95 data-[placement=bottom]:slide-in-from-top-2 data-[placement=left]:slide-in-from-right-2 data-[placement=right]:slide-in-from-left-2 data-[placement=top]:slide-in-from-bottom-2",
         className
       )
     }
