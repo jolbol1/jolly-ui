b9d606b16229b9b1ace0747e7ae422e16b0240cb
jolbol1 <github@jamesshopland.com>
2023-11-28 20:53:12 +0000

    docs: tooltip fix

--- apps/www/registry/new-york/example/tooltip-demo.tsx
+++ apps/www/registry/new-york/example/tooltip-demo.tsx
@@ -1,13 +1,15 @@
+import { Pencil1Icon } from "@radix-ui/react-icons"
+
 import { Button } from "@/registry/new-york/ui/button"
 import { Tooltip, TooltipTrigger } from "@/registry/new-york/ui/tooltip"
 
 export default function TooltipDemo() {
   return (
     <TooltipTrigger>
-      <Button variant="outline">Hover</Button>
-      <Tooltip>
-        <p>Add to library</p>
-      </Tooltip>
+      <Button variant="outline" size="icon">
+        <Pencil1Icon className="h-4 w-4" />
+      </Button>
+      <Tooltip>Edit</Tooltip>
     </TooltipTrigger>
   )
 }
