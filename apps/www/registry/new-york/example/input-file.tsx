3cac1961fb9b808d8d4f8f8b6edd05d33e65b62d
jolbol1 <github@jamesshopland.com>
2023-11-28 17:49:19 +0000

    fix: input examples

--- apps/www/registry/new-york/example/input-file.tsx
+++ apps/www/registry/new-york/example/input-file.tsx
@@ -5,12 +5,9 @@ import { Label } from "@/registry/new-york/ui/label"
 
 export default function InputFile() {
   return (
-    <TextField
-      type="file"
-      className="grid w-full max-w-sm items-center gap-1.5"
-    >
+    <div className="grid w-full max-w-sm items-center gap-1.5">
       <Label htmlFor="picture">Picture</Label>
-      <Input id="picture" />
-    </TextField>
+      <Input type="file" id="picture" />
+    </div>
   )
 }
