1c30756281fcc6b4f483a92e022f8c2c001e22c3
jolbol1 <github@jamesshopland.com>
2024-01-06 17:34:43 +0000

    refactor: update metadata

--- apps/docs/config/site.ts
+++ apps/docs/config/site.ts
@@ -3,7 +3,7 @@ export const siteConfig = {
   url: "https://jollyui.dev",
   ogImage: "https://jollyui.dev/og.jpg",
   description:
-    "shadcn/ui compatible react aria components that you can copy and paste into your apps. Accessible. Customizable. Open Source.",
+    "shadcn/ui compatible react aria components that you can copy and paste into your apps. Accessible. Customizable. Open Source. Formerly known as shadcn-aria.",
   links: {
     twitter: "https://twitter.com/jollyshopland",
     github: "https://github.com/jolbol1/shadcn-aria",
