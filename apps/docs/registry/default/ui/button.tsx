c97248f9225d94e47390b0bacf011b3be6f24eab
jolbol1 <github@jamesshopland.com>
2024-01-13 09:42:27 +0000

    fix(button): disabled states for variants

--- apps/docs/registry/default/ui/button.tsx
+++ apps/docs/registry/default/ui/button.tsx
@@ -10,7 +10,7 @@ import {
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
   {
     variants: {
       variant: {
