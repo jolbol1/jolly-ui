528d7fce36858bb76f72fe6ff485673132fdcbbf
jolbol1 <github@jamesshopland.com>
2024-07-15 22:45:21 +0100

    update announcement

--- apps/docs/components/announcement.tsx
+++ apps/docs/components/announcement.tsx
@@ -11,9 +11,11 @@ export function Announcement() {
       href="/docs/changelog"
       className="inline-flex items-center rounded-lg bg-muted px-3 py-1 text-sm font-medium"
     >
-      🎉 <Separator className="mx-2 h-4" orientation="vertical" />{" "}
-      <span className="sm:hidden">Color Pickers and Submenu!</span>
-      <span className="hidden sm:inline">Color Pickers and Submenu!</span>
+      🎉 <Separator className="mx-2 h-4" orientation="vertical" />
+      <span className="sm:hidden">New Components!</span>
+      <span className="hidden sm:inline">
+        Color Pickers, Submenu, and more!
+      </span>
       <ArrowRightIcon className="ml-1 size-4" />
     </Link>
   )
