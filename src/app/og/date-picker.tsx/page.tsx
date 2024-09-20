39a3891da41f5e1d2ff14adba2e4e1247dba22ac
jolbol1 <github@jamesshopland.com>
2024-09-20 19:51:12 +0100

    change proxy page

--- src/app/og/date-picker.tsx/page.tsx
+++ src/app/og/date-picker.tsx/page.tsx
@@ -24,7 +24,20 @@ export default async function ProxyPage() {
   return (
     <main className="flex h-screen w-screen items-center justify-center">
       <div className="mx-5 w-full max-w-lg overflow-hidden rounded-lg border border-gray-200 sm:mx-0">
-        you shouldnt be here
+        <img
+          src={"/date-picker.png"}
+          alt={"Datepicker component"}
+          className="w-full object-cover"
+        />
+        <div className="flex space-x-3 bg-gray-100 p-5">
+          <div className="flex flex-col space-y-3">
+            <h1 className="font-bold text-gray-700">DatePicker</h1>
+            <p className="text-sm text-gray-500">
+              install with npx shadcn add
+              https://jollyui.dev/default/date-picker
+            </p>
+          </div>
+        </div>
       </div>
     </main>
   )
