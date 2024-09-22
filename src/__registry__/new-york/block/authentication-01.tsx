04c2b067b2c4cf003420d48d302ae0887b3129df
jolbol1 <github@jamesshopland.com>
2024-09-22 22:40:51 +0100

    update registry

--- src/__registry__/new-york/block/authentication-01.tsx
+++ src/__registry__/new-york/block/authentication-01.tsx
@@ -10,7 +10,7 @@ import { Input, TextField } from "@/registry/new-york/ui/textfield"
 export const description =
   "A login page with two columns. The first column has the login form with email and password. There's a Forgot your passwork link and a link to sign up if you do not have an account. The second column has a cover image."
 
-export function Dashboard() {
+export default function Authentication() {
   return (
     <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
       <div className="flex items-center justify-center py-12">
--- src/registry/registry-blocks.ts
+++ src/registry/registry-blocks.ts
@@ -4,6 +4,7 @@ export const blocks: Registry = [
   {
     name: "authentication-01",
     type: "registry:block",
+    dependencies: ["react-aria-components"],
     registryDependencies: ["button", "textfield", "field"],
     files: ["block/authentication-01.tsx"],
     category: "Authentication",
