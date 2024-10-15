b9d9a07ae78e295c6f588f3c17e057db564e1821
jolbol1 <github@jamesshopland.com>
2024-10-15 02:27:05 +0100

    remove uneeded values

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -23,11 +23,6 @@ module.exports = {
     "",
     "^[./]",
   ],
-  importOrderSeparation: false,
-  importOrderSortSpecifiers: true,
-  importOrderBuiltinModulesToTop: true,
   importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
-  importOrderMergeDuplicateImports: true,
-  importOrderCombineTypeAndValueImports: true,
   plugins: ["@ianvs/prettier-plugin-sort-imports"],
 }
