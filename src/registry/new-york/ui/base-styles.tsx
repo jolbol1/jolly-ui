38a05966e8ae0296307f7bdbead155bcf832d33b
jolbol1 <github@jamesshopland.com>
2024-10-18 20:00:48 +0100

    button test

---
+++ src/registry/new-york/ui/base-styles.tsx
@@ -0,0 +1,12 @@
+import { tv } from "tailwind-variants"
+
+const focusRing = tv({
+  base: "outline-none ring-offset-background transition-colors",
+  variants: {
+    isFocusVisible: {
+      true: "outline-none ring-2 ring-ring ring-offset-1",
+    },
+  },
+})
+
+export { focusRing }
--- src/registry/new-york/ui/button.tsx
+++ src/registry/new-york/ui/button.tsx
@@ -1,68 +1,61 @@
 "use client"
 
 import * as React from "react"
-import { cva, type VariantProps } from "class-variance-authority"
 import {
   Button as AriaButton,
   composeRenderProps,
   type ButtonProps as AriaButtonProps,
 } from "react-aria-components"
+import { tv, VariantProps } from "tailwind-variants"
 
-import { cn } from "@/lib/utils"
+import { focusRing } from "@/registry/new-york/ui/base-styles"
 
-const buttonVariants = cva(
-  [
-    "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors",
-    /* Disabled */
-    "data-[disabled]:pointer-events-none data-[disabled]:opacity-50 ",
-    /* Focus Visible */
-    "data-[focus-visible]:outline-none data-[focus-visible]:ring-1 data-[focus-visible]:ring-ring ",
-    /* Resets */
-    "focus-visible:outline-none",
-  ],
-  {
-    variants: {
-      variant: {
-        default:
-          "bg-primary text-primary-foreground shadow data-[hovered]:bg-primary/90",
-        destructive:
-          "bg-destructive text-destructive-foreground shadow-sm data-[hovered]:bg-destructive/90",
-        outline:
-          "border border-input bg-background shadow-sm  data-[hovered]:bg-accent data-[hovered]:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground shadow-sm data-[hovered]:bg-secondary/80",
-        ghost: "data-[hovered]:bg-accent data-[hovered]:text-accent-foreground",
-        link: "text-primary underline-offset-4 data-[hovered]:underline",
-      },
-      size: {
-        default: "h-9 px-4 py-2",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
-        icon: "size-9",
-      },
+const buttonVariants = tv({
+  extend: focusRing,
+  base: "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors",
+  variants: {
+    variant: {
+      default:
+        "bg-primary shadow text-primary-foreground data-[hovered]:bg-primary/90",
+      destructive:
+        "bg-destructive shadow-sm text-destructive-foreground data-[hovered]:bg-destructive/90",
+      outline:
+        "border shadow-sm border-input bg-background data-[hovered]:bg-accent data-[hovered]:text-accent-foreground",
+      secondary:
+        "bg-secondary shadow-sm text-secondary-foreground data-[hovered]:bg-secondary/80",
+      ghost: "data-[hovered]:bg-accent data-[hovered]:text-accent-foreground",
+      link: "text-primary underline-offset-4 data-[hovered]:underline",
     },
-    defaultVariants: {
-      variant: "default",
-      size: "default",
+    size: {
+      default: "h-9 px-4 py-2",
+      sm: "h-8 rounded-md px-3 text-xs",
+      lg: "h-10 rounded-md px-8",
+      icon: "size-9",
     },
-  }
-)
+    isDisabled: {
+      true: "pointer-events-none opacity-50",
+    },
+    isPending: {
+      true: "pointer-events-none opacity-50",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+    isDisabled: false,
+    isPending: false,
+  },
+})
 
-interface ButtonProps
-  extends AriaButtonProps,
-    VariantProps<typeof buttonVariants> {}
+type ButtonVariants = VariantProps<typeof buttonVariants>
+
+interface ButtonProps extends AriaButtonProps, ButtonVariants {}
 
 const Button = ({ className, variant, size, ...props }: ButtonProps) => {
   return (
     <AriaButton
-      className={composeRenderProps(className, (className) =>
-        cn(
-          buttonVariants({
-            variant,
-            size,
-            className,
-          })
-        )
+      className={composeRenderProps(className, (className, renderProps) =>
+        buttonVariants({ ...renderProps, variant, size, className })
       )}
       {...props}
     />
@@ -70,4 +63,4 @@ const Button = ({ className, variant, size, ...props }: ButtonProps) => {
 }
 
 export { Button, buttonVariants }
-export type { ButtonProps }
+export type { ButtonProps, ButtonVariants }
